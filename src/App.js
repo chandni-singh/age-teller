7bfcc6971af6b18c08b1e508cdfc5ce286891f69
Chandni Singh <chandni1141996@gmail.com>
2021-08-11 23:47:57 +0530

    app functioning logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import AppStats from './AppStats';
+import './App.css';
 
-export default class App extends React.Component{
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            newDate: '',
+            birthday: '1996-04-11'
+        }
+    }
+
+    changeBirthday() {
+        console.log(this.state);
+        this.setState({birthday: this.state.newDate});
+    }
+
     render() {
         return (
-            <div>
-                AgeTeller App
+            <div className='App'>
+                <Form inline>
+                    <h2>Enter your Birthday!</h2>
+                    <FormControl type="Date" onChange={e => this.setState({newDate: e.target.value})} />
+                    {' '}
+                    <Button onClick={() => this.changeBirthday()}>
+                        Submit
+                    </Button>
+                    <AppStats date={this.state.birthday} />
+                </Form>
             </div>
         )
     }
---
+++ src/AppStats.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class AppStats extends React.Component {
+
+    timeSince(date) {
+        const today = new Date().getTime();
+        const birthdayDate = new Date(date).getTime();
+        let difference = Math.abs(today - birthdayDate);
+
+        let days = Math.floor(difference / (1000*3600*24));
+        let years = Math.floor(days/365);
+        days -= years*365;
+        let months = Math.floor(days/30);
+        days -= months*30;
+
+        return `${years} years, ${months} months and ${days} days`
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>{this.props.date}</h3>
+                <h4>Congrats on {this.timeSince(this.props.date)}!</h4>
+            </div>
+        );
+    }
+}
